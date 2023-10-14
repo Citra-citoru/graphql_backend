ccac4c680963a2c0c6ab5ca90c7fa10c2327f55f
citra-citoru <citra.rahman@happy5.co>
2023-10-14 21:29:13 +0300

    change github action test to rspec

--- spec/requests/ping_spec.rb
+++ spec/requests/ping_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe "Pings", type: :request do
   describe "/api/v1/ping" do
     it "returns ok" do
       get "/api/v1/ping"
-      binding.pry
       expect(response).to have_http_status(:ok)
     end
   end
