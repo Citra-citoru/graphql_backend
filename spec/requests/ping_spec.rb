59c8ba6407d088ad069c77a2f4e919d06433bb26
citra-citoru <citra.rahman@happy5.co>
2023-10-14 21:22:23 +0300

    add pry:

--- spec/requests/ping_spec.rb
+++ spec/requests/ping_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Pings", type: :request do
   describe "/api/v1/ping" do
     it "returns ok" do
       get "/api/v1/ping"
+      binding.pry
       expect(response).to have_http_status(:ok)
     end
   end
