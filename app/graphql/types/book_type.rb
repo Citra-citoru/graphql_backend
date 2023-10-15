665e3036733c7f29ababcef8b5d87abb48538415
citra-citoru <citra.rahman@happy5.co>
2023-10-15 20:51:48 +0300

    finish add author_type & book_type

---
+++ app/graphql/types/book_type.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Types
+  class BookType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :pages, Integer
+    field :rating, Float
+    field :year_published, Integer
+    field :author_id, Integer, null: false
+  end
+end
